e981e02b7d015351360e0e0d7aecd5112a4db618
rmirkhaidarov <radmir.mirkhaidarov@gmail.com>
2024-02-08 13:38:52 +0600

    feat: add swagger and fastify

---
+++ src/core/constants/swagger.ts
@@ -0,0 +1,5 @@
+export const SWAGGER_PATH = 'swagger'
+
+export const SWAGGER_TITLE = 'financial server'
+
+export const SWAGGER_VERSION = '1.0'
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { SWAGGER_PATH, SWAGGER_TITLE, SWAGGER_VERSION } from '@core/constants/swagger'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  const swaggerConfig = new DocumentBuilder().setTitle(SWAGGER_TITLE).setVersion(SWAGGER_VERSION).build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+
+  SwaggerModule.setup(SWAGGER_PATH, app, swaggerDocument)
   await app.listen(3000)
 }
 bootstrap()
