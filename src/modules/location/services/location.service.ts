30e3a8ce8d5c6acab65abeb4544b0fe2afe33b9a
rmirkhaidarov <radmir.mirkhaidarov@gmail.com>
2024-06-16 17:20:13 +0500

    feat: (location): add ability save country without any transaction' record

--- src/modules/location/services/location.service.ts
+++ src/modules/location/services/location.service.ts
@@ -1,15 +1,11 @@
 import { v4 as uuid } from 'uuid'
 import { Repository } from 'typeorm'
-import type { FindManyOptions } from 'typeorm'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { FinancialRecord } from '@modules/transaction/core/entity'
-import { getCurrentMonth } from '@shared/utils'
 import { ExceptionService } from '@core/modules/exception'
 import { AddCountryDto, AddCityDto } from '../core/dto'
 import { City, Country } from '../core/entity'
 import { LocationExceptionMessages } from '../core/exception-messages'
-import { TransactionType } from '@core/enums/transaction-type'
 
 @Injectable()
 export class LocationService {
@@ -19,24 +15,13 @@ export class LocationService {
     private readonly cityRepository: Repository<City>,
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>,
-    @InjectRepository(FinancialRecord)
-    private readonly financialRecordRepository: Repository<FinancialRecord>,
   ) {}
 
   async addCountry({ name }: AddCountryDto) {
     try {
-      const expenseFinancialRecords = await this.getExpenseFinancialRecords()
-
-      if (!expenseFinancialRecords.length) {
-        this.exceptionService.notFound(LocationExceptionMessages.FINANCIAL_RECORD_NOT_FOUND)
-      }
-
-      expenseFinancialRecords.map(async (financialRecord) => {
-        await this.countryRepository.save({
-          id: uuid(),
-          name,
-          financialRecord,
-        })
+      await this.countryRepository.save({
+        id: uuid(),
+        name,
       })
 
       return this.exceptionService.success({
@@ -50,42 +35,21 @@ export class LocationService {
 
   async addCity({ name }: AddCityDto) {
     try {
-      const expenseFinancialRecords = await this.getExpenseFinancialRecords(true)
+      const [lastCountry] = await this.countryRepository.find({ take: 1, order: { createdAt: 'DESC' } })
 
-      if (!expenseFinancialRecords.length) {
-        this.exceptionService.notFound(LocationExceptionMessages.FINANCIAL_RECORD_NOT_FOUND)
+      if (!lastCountry) {
+        this.exceptionService.notFound(LocationExceptionMessages.COUNTRY_NOT_FOUND)
       }
 
-      expenseFinancialRecords.map(async ({ countries }) => {
-        const lastCountry = countries.at(0)
-
-        if (lastCountry) {
-          await this.cityRepository.save({
-            id: uuid(),
-            name,
-            country: lastCountry,
-          })
-        }
+      await this.cityRepository.save({
+        id: uuid(),
+        name,
+        country: lastCountry,
       })
 
       return this.exceptionService.success({ message: LocationExceptionMessages.ADD_CITY_SUCCESS, data: name })
     } catch (error: unknown) {
       this.exceptionService.internalServerError(error)
     }
-  }
-
-  private async getExpenseFinancialRecords(withCountry = false) {
-    const options: FindManyOptions<FinancialRecord> = {
-      where: {
-        transaction: {
-          type: TransactionType.EXPENSE,
-        },
-        recordTitle: getCurrentMonth(),
-      },
-      relations: withCountry ? { countries: true } : null,
-      order: withCountry ? { countries: { updatedAt: 'DESC', createdAt: 'DESC' } } : null,
-    }
-
-    return this.financialRecordRepository.find(options)
   }
 }
